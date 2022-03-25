b62583779bc0013d33f789156d72420e5b79c6ed
Marcin Skowyra <marcin.skowyra@netguru.com>
2022-03-25 14:05:04 +0100

    Initial Configuration

---
+++ packages/ui/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// "packages/ui/**/*.{js,ts,jsx,tsx}": [
+//   "eslint --fix"
+// ],
+// "*.json": [
+//   "prettier --write"
+// ]
